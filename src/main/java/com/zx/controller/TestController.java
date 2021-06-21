80d1ab4858fb1ab1e40c1c6d911742b9102013bd
melon2624 <1415458002@qq.com>
2021-06-22 00:58:11 +0800

    加了个String

--- src/main/java/com/zx/controller/TestController.java
+++ src/main/java/com/zx/controller/TestController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+	public String a="zx";
+
 
     @RequestMapping("/zx/test")
     public  String test(){
