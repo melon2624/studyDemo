47dd1e1cf12bd7951d273fb2572d518f80c3e2fb
melon2624 <1415458002@qq.com>
2021-06-22 00:50:28 +0800

    增加testURL

--- src/main/java/com/zx/controller/TestController.java
+++ src/main/java/com/zx/controller/TestController.java
@@ -1,4 +1,15 @@
 package com.zx.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+
+    @RequestMapping("/zx/test")
+    public  String test(){
+
+        return null;
+    }
 }
