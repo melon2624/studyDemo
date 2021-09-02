e2198cef93b7eb1cfefc3fc691d186e8bff1571a
zhangxin <1415458002@qq.com>
2021-09-03 01:16:59 +0800

    zhangxin

---
+++ src/main/java/com/zx/leetcode/二叉树/RecursiveTraversalBT.java
@@ -0,0 +1,66 @@
+package com.zx.leetcode.二叉树;
+
+/**
+ * 二叉树的递归遍历
+ *
+ * @author : zhangxin
+ * @date : 2021-08-31 14:30
+ **/
+public class RecursiveTraversalBT {
+
+    public static class Node {
+        int value;
+        public Node left;
+        public Node right;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    //先序遍历打印节点
+    public static void pre(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.println(head.value);
+        pre(head.left);
+        pre(head.right);
+    }
+
+    public  static  void  in(Node head){
+
+        if (head==null){
+            return;
+        }
+        in(head.left);
+        System.out.println(head.value);
+        in(head.right);
+    }
+
+    public  static  void  pos(Node head){
+        if (head==null){
+            return;
+        }
+        pos(head.left);
+        pos(head.right);
+        System.out.println(head.value);
+    }
+
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.left = new Node(2);
+        head.right = new Node(3);
+        head.left.left = new Node(4);
+        head.left.right = new Node(5);
+        head.right.left = new Node(6);
+        head.right.right = new Node(7);
+        pre(head);
+        System.out.println("========");
+        in(head);
+        System.out.println("========");
+        pos(head);
+    }
+
+}
---
+++ src/main/java/com/zx/leetcode/链表/SmallerEqualBigger.java
@@ -0,0 +1,34 @@
+package com.zx.leetcode.链表;
+
+/**
+ * 将单向链表按某值划分成左边小、中间相等、右边大的形式
+ * 1）把链表放入数组里，在数组上做partition（笔试用）
+ * 2）分成小、中、大三部分，再把各个部分之间串起来（面试用）
+ *
+ * @author : zhangxin
+ * @date : 2021-08-30 19:34
+ **/
+public class SmallerEqualBigger {
+
+
+    public static class Node {
+
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+
+    }
+
+    public static Node listPartition1(Node head) {
+
+        return null;
+    }
+
+    public static Node listPartition2(Node head) {
+        return null;
+    }
+
+}
