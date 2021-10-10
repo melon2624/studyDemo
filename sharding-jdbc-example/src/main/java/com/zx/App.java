09815acb04013eecb5ef398a3b30a20bfc6a41bd
zhangxin <772105823@qq.com>
2021-10-11 01:46:29 +0800

    sharding

---
+++ sharding-jdbc-example/src/main/java/com/zx/App.java
@@ -0,0 +1,12 @@
+package com.zx;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangxin
+ * @date 2021/10/11 1:27
+ */
+@SpringBootApplication
+public class App {
+
+}
