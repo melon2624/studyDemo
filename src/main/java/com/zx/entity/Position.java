8db8d35257bace348aee7f46abed7201d6b1e987
zhangxin <772105823@qq.com>
2021-10-18 23:27:59 +0800

    test

---
+++ src/main/java/com/zx/entity/Position.java
@@ -0,0 +1,47 @@
+package com.zx.entity;
+
+/**
+ * @author  zhangxin
+ * @date  2021/10/11 1:20
+ */
+public class Position {
+    private Long id;
+
+    private String name;
+
+    private String salary;
+
+    private String city;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
