44586aac5d97f85cee5741b72954c496ddf786f2
zhangxin <1415458002@qq.com>
2022-10-06 23:52:27 +0800

    seata

---
+++ seata-all/one/src/main/java/com/zx/one/OneApplication.java
@@ -0,0 +1,24 @@
+package com.zx.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class OneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneApplication.class, args);
+		//System.out.println("zxxxxxxxxx");
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+
+}
---
+++ seata-all/three/src/test/java/com/zx/three/ThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zx.three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
